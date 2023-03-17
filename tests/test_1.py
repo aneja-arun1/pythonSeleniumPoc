2b61744237f856ef31603025fa249332bc414a48
aneja-arun1 <aneja.arun1@gmail.com>
2023-03-17 03:32:52 +0100

    Adding initial test for opening a website.

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,8 +1,19 @@
 import pytest
 from assertpy import assert_that
+import selenium
+from selenium import webdriver
+import time
 
 
 @pytest.mark.smoke
 def test_abc():
     abc = "123"
     assert_that(abc).is_equal_to("123")
+
+
+@pytest.mark.smoke
+def test_browser():
+    browser = webdriver.Chrome()
+    browser.get("https://duckduckgo.com/")
+    time.sleep(10)
+    browser.quit()
